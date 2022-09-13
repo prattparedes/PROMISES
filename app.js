e0320bb2907bb42ce3db337b13b84fd34c6a7b79
Pratt <pratt.paredes@gmail.com>
2022-09-12 22:58:16 -0500

    calling and creating promises

--- app.js
+++ app.js
@@ -40,3 +40,63 @@ async function main() {
 }
 
 main();
+
+
+console.log('<----------------------------------------COMO CREAR UNA PROMESA------------------------------->')
+// HOW TO CREATE A PROMISE
+
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {                  // Con esto hacemos que demore 2 segundos en aparecer el promise
+            resolve('VIP')
+        }, 2000);
+    })
+}
+
+console.log(getSubscriptionStatus())
+
+//calling the promise (Sale VIP)
+
+// 1. Usando Then 1
+
+getSubscriptionStatus().then((response) => console.log(response))
+
+
+// 2. Usando Async/Await
+async function funciónPromise() {
+    console.log(await getSubscriptionStatus())
+}
+
+funciónPromise()
+
+// Crear un promise para que aparezca la ultima clase y el ultimo dia de conexion y que demore 3 segundos en cargar
+
+const lastClassRef = document.querySelector('.lastclass')
+const lastConnectRef = document.querySelector('.lastconnect')
+
+function getLastClass() {
+    return new Promise ((resolve, reject) => {
+        resolve('Flexbox Class')
+    })
+}
+
+function getLastConnection() {
+    return new Promise ((resolve, reject) => {
+        setTimeout ( () => {
+            resolve('12/09/2022')
+        }, 3000)   
+    })
+}
+
+//Calleando con then y otro con async/await
+
+getLastClass().then((data) => {
+    lastClassRef.innerHTML = data
+})
+
+
+async function getLastConnectionREF() {
+    lastConnectRef.innerHTML = await getLastConnection()
+}
+
+getLastConnectionREF();
